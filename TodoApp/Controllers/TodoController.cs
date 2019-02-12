968162855d0b48ed0619c988477a52da83a0af03
Zoltán Dávid Takács <takacsz@sensenet.com>
2019-02-12 09:24:04 +0100

    Repo init with basic functionality

---
+++ TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : ApiController
+    {
+        private List<Todo> todoList = new List<Todo>
+        {
+            new Todo(name: "Workout", priority: "minor"),
+            new Todo(id: 1, name: "Go shopping", priority: "major"),
+            new Todo(id: 2, name: "Learn .NET", priority: "critical", status: "in progress", deadline: new DateTime(2019, 05, 11))
+        };
+
+        // GET: api/todo
+        public string Get()
+        {
+            return JsonConvert.SerializeObject(todoList);
+        }
+
+        // GET: api/todo/5
+        public string Get(int id)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(todoList[id]);
+            } catch (ArgumentOutOfRangeException)
+            {
+                return "Error: Invalid id";
+            }
+        }
+
+        // POST: api/todo
+        public void Post([FromBody]string value)
+        {
+            Todo newItem = JsonConvert.DeserializeObject<Todo>(value);
+
+            todoList.Add(newItem);
+        }
+
+        // PATCH: api/todo/5
+        public void Patch(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE: api/todo/5
+        public void Delete(int id)
+        {
+            todoList.RemoveAt(id);
+        }
+    }
+}
---
+++ TodoApp/Models/Todo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoApp.Models
+{
+    public enum CategoryEnum {
+        BUG,
+        TASK,
+        EPIC
+    }
+
+    public class Todo
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public String Priority { get; set; }
+        public String Responsible { get; set; }
+        public DateTime Deadline { get; set; }
+        public String Status { get; set; }
+        public CategoryEnum Category { get; set; }
+        public int ParentId { get; set; }
+
+        public Todo(
+            String name,
+            String priority,
+            int id  = 0,
+            String description = "",
+            String responsible = "",
+            DateTime? deadline = null,
+            String status = "todo",
+            CategoryEnum category = CategoryEnum.TASK,
+            int parent = 0
+        ) {
+            if (deadline == null)
+            {
+                deadline = DateTime.Now.AddDays(1);
+            }
+
+            Name = name;
+            Responsible = responsible;
+            Id = id;
+            Description = description;
+            Deadline = (DateTime)deadline;
+            Status = status;
+            Category = category;
+            ParentId = parent;
+        }
+    }
+}
